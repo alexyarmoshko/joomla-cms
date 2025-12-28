4cfb88186c19fef3935d962f150193987f1f44d4
Alex Yarmoshko <48326842+alexyarmoshko@users.noreply.github.com>
2025-12-28 20:41:55 +0000

    Five and fourth stages of developing YSTides module with codex - change styles and metadata

--- modules/mod_ystides/tmpl/default.php
+++ modules/mod_ystides/tmpl/default.php
@@ -30,35 +30,35 @@ $rowsData       = $rows ?? [];
 			<?php echo htmlspecialchars($fetchErrorMessage, ENT_QUOTES, 'UTF-8'); ?>
 		</div>
 	<?php else : ?>
-	<table class="table table-striped mod-ystides__table">
+	<table class="table table-striped mod-ystides-table">
 		<thead>
 			<tr>
-				<th colspan="3" class="mod-ystides__station">
+				<th colspan="3" class="mod-ystides-table-header">
 					<?php echo htmlspecialchars($stationHeader, ENT_QUOTES, 'UTF-8'); ?>
 				</th>
 			</tr>
 			<tr>
-				<th colspan="3" class="mod-ystides__range">
+				<th colspan="3" class="mod-ystides-table-subheader">
 					<?php echo Text::sprintf('MOD_YSTIDES_DATE_RANGE', htmlspecialchars($rangeStart, ENT_QUOTES, 'UTF-8'), htmlspecialchars($rangeEnd, ENT_QUOTES, 'UTF-8')); ?>
 				</th>
 			</tr>
 			<tr>
-				<th scope="col"><?php echo Text::_('MOD_YSTIDES_HEADING_TIME'); ?></th>
-				<th scope="col"><?php echo Text::_('MOD_YSTIDES_HEADING_WLM'); ?></th>
+				<th scope="col" class="mod-ystides-table-subheader-col1"><?php echo Text::_('MOD_YSTIDES_HEADING_TIME'); ?></th>
+				<th scope="col" class="mod-ystides-table-subheader-col2"><?php echo Text::_('MOD_YSTIDES_HEADING_WLM'); ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php if (empty($rowsData)) : ?>
-				<tr class="mod-ystides__empty">
+				<tr class="mod-ystides-empty">
 					<td colspan="3"><?php echo Text::_('MOD_YSTIDES_NO_DATA'); ?></td>
 				</tr>
 			<?php else : ?>
 				<?php foreach ($rowsData as $row) : ?>
 					<tr>
-						<td title="<?php echo htmlspecialchars($row['tooltip'], ENT_QUOTES, 'UTF-8'); ?>">
+						<td class="mod-ystides-table-data-col1" title="<?php echo htmlspecialchars($row['tooltip'], ENT_QUOTES, 'UTF-8'); ?>">
 							<?php echo htmlspecialchars($row['time'], ENT_QUOTES, 'UTF-8'); ?>
 						</td>
-						<td>
+						<td class="mod-ystides-table-data-col2" >
 							<?php echo htmlspecialchars($row['symbol'] . ' ' . $row['wlm'], ENT_QUOTES, 'UTF-8'); ?>
 						</td>
 					</tr>
